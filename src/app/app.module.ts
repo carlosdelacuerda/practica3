7b1c1f86d0396e5dac72f816d161eee6e0c85c61
Carlos de la Cuerda <carlosdelacuerda@gmail.com>
2021-02-17 08:52:38 +0100

    Inicio de proyecto. Creados los componente en blanco: blog y new

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NewComponent } from './components/new/new.component';
+import { BlogComponent } from './components/blog/blog.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NewComponent,
+    BlogComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
